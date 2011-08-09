dd9782bab2db4c35d4fefc41748c137c74d17dcd
Torsten Schönfeld <kaffeetisch@gmx.de>
2011-08-09 14:01:36 +0200

    Tweak doc summary line

--- lib/Glib/Object/Introspection.pm
+++ lib/Glib/Object/Introspection.pm
@@ -89,7 +89,7 @@ __END__
 
 =head1 NAME
 
-Glib::Object::Introspection - Dynamically create language bindings
+Glib::Object::Introspection - Dynamically create Perl language bindings
 
 =head1 SYNOPSIS
 
